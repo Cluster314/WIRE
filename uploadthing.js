dd4ca3cabb825560a2e89ad17ca1a1444903659b
Abdulrahman Mohammed <abdulrahmanmohammed7years@gmail.com>
2026-09-09 19:21:44 -0400

    Refactor messageAttachment to use blob configuration

--- uploadthing.js
+++ uploadthing.js
@@ -3,15 +3,12 @@ import { createUploadthing } from "uploadthing/server";
 const f = createUploadthing();
 
 export const uploadRouter = {
-  messageAttachment: f([
-    "image",
-    "video",
-    "audio",
-    "pdf",
-    "text",
-    "blob",
-  ])
-    .onUploadComplete(({ file }) => {
-      console.log("WIRE attachment uploaded:", file.name);
-    }),
+  messageAttachment: f({
+    blob: {
+      maxFileSize: "20MB",
+      maxFileCount: 1
+    }
+  }).onUploadComplete(({ file }) => {
+    console.log("WIRE attachment uploaded:", file.name);
+  })
 };
