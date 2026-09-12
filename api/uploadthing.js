46e50e9e88da1fa3782a46ca515d587139415e07
Abdulrahman Mohammed <abdulrahmanmohammed7years@gmail.com>
2026-09-11 20:25:16 -0400

    Implement CORS support in uploadthing.js
    Added CORS support to the upload handler for OPTIONS requests and set appropriate headers for GET and POST methods.

--- api/uploadthing.js
+++ api/uploadthing.js
@@ -5,8 +5,41 @@ const handler = createRouteHandler({
   router: uploadRouter
 });
 
-export default {
-  fetch(request) {
-    return handler(request);
+export default async function handlerWithCors(request) {
+  const origin = request.headers.get("origin");
+
+  if (request.method === "OPTIONS") {
+    return new Response(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": origin === "https://cluster314.github.io" ? origin : "https://cluster314.github.io",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, x-uploadthing-package",
+        "Access-Control-Max-Age": "86400"
+      }
+    });
   }
-};
+
+  const response = await handler(request);
+
+  const headers = new Headers(response.headers);
+
+  headers.set(
+    "Access-Control-Allow-Origin",
+    origin === "https://cluster314.github.io"
+      ? origin
+      : "https://cluster314.github.io"
+  );
+
+  headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, x-uploadthing-package"
+  );
+
+  return new Response(response.body, {
+    status: response.status,
+    statusText: response.statusText,
+    headers
+  });
+}
